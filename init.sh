289d00284dc36581e8541f01d87b73138994156b
radtkedev <mail@radtke.dev>
2022-02-17 07:58:04 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo dnf install git ansible -y
+sudo dnf install git ansible python3-libselinux -y
 git clone https://github.com/radtkedev/fedora
 ansible-playbook -c local -i localhost, fedora/ansible.yml
 rm -rf fedora
